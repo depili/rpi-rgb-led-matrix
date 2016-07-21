254b2a0b662533fe42832ae388d2688d14c86e62
Vesa-Pekka Palmu <vpalmu@depili.fi>
2016-07-21 23:51:47 +0300

    Move the scroll method to the matrix class

--- ruby/matrix-test.rb
+++ ruby/matrix-test.rb
@@ -12,39 +12,6 @@ host = ARGV[0]
 rows = ARGV[1].to_i
 cols = ARGV[2].to_i
 
-def scroll(bitmap, color, i, x, y, l)
-	s = bitmap[0].size
-	if s < l
-		# Bitmap is smaller than requested scroll area, just print the text
-		bitmap.each_with_index do |row, r|
-			row.each_with_index do |b, c|
-				@matrix.setPixel(r+x,c+y,color) if b
-			end
-		end
-		return
-	end
-	
-	i = i%s
-	size = s - i
-	if size >= l
-		bitmap.each_with_index do |row, r|
-			row[i,l].each_with_index do |b, c|
-				@matrix.setPixel(r+x,c+y,color) if b
-			end
-		end
-	else
-		bitmap.each_with_index do |row, r|
-			row[i,l].each_with_index do |b, c|
-				@matrix.setPixel(r+x,c+y,color) if b
-			end
-			
-			row[0,l - size].each_with_index do |b, c|
-				@matrix.setPixel(r+x,c+y + size,color) if b
-			end
-		end
-	end
-end
-
 font = BdfFont.new('../fonts/7x13B.bdf')
 
 @matrix = ZmqMatrix.new(host, rows, cols)
@@ -67,7 +34,7 @@ bitmap = font.text_to_bitmap("Foo")
 
 500.times do
 	@matrix.matrix = background.dup
-	scroll(bitmap, [0,255,0], 23, 0, 0, 128)
+	@matrix.scroll(bitmap, [0,255,0], 23, 0, 0, 128)
 	@matrix.send
 	sleep 1 / 200.0	
 end
@@ -77,9 +44,9 @@ bitmap = font.text_to_bitmap(text)
 time = Benchmark.realtime do
 	(0..l*5).each do |i|
 		@matrix.matrix = background.dup
-		scroll(bitmap, [0,255,0], i, 16, 32, 64)
+		@matrix.scroll(bitmap, [0,255,0], i, 16, 32, 64)
 		@matrix.send
-		sleep 1 / 200.0
+		sleep 1 / 150.0
 	end
 end
 puts "Total time: #{time}"
--- ruby/zmq_matrix.rb
+++ ruby/zmq_matrix.rb
@@ -28,4 +28,33 @@ class	ZmqMatrix
 	def fill(color)
 		@matrix.fill(nil, @rows * @columns * 3) {|i| color[i%3]}
 	end
+	
+	def scroll(bitmap, color, i, x, y, l)
+		s = bitmap[0].size
+		if s < l
+			# Bitmap is smaller than requested scroll area, just print the text
+			draw_bitmap(bitmap, color, 0, x, y, l)
+			return
+		end
+	
+		i = i%s
+		size = s - i
+		if size >= l
+			draw_bitmap(bitmap, color, i, x, y, l)
+		else
+			draw_bitmap(bitmap, color, i, x, y, l)
+			draw_bitmap(bitmap, color, 0, x, y + size, l - size)
+		end
+	end
+	
+	private
+	
+	def draw_bitmap(bitmap, color, i, x, y, l)
+		bitmap.each_with_index do |row, r|
+			row[i,l].each_with_index do |b, c|
+				setPixel(r+x,c+y,color) if b
+			end
+		end
+	end
+	
 end
